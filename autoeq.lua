5c9db44a38cb086eda76ce252218920b636f0482
Byebaih1 <byebaih1@gmail.com>
2024-12-02 21:10:31 +0700

    Update autoeq.lua

--- autoeq.lua
+++ autoeq.lua
@@ -1,13 +1,35 @@
-function AutoEquip()
-    while true do wait()
-    a = "Kabucha"
-        if game.Players.LocalPlayer.Backpack:FindFirstChild(a) then
-            Tool = game.Players.LocalPlayer.Backpack:FindFirstChild(a)
-            wait(.1)
-            game.Players.LocalPlayer.Character.Humanoid:EquipTool(Tool)
-        end
-    end 
-end
+local Players = game:GetService("Players")
 
+function AutoEquip()
+    local player = Players.LocalPlayer
+    local toolName = "Kabucha"
+
+    -- Function to equip the tool if it's found in the backpack
+    local function equipTool()
+        local backpack = player.Backpack
+        local character = player.Character
+
+        -- Check if the tool exists in the backpack
+        if backpack:FindFirstChild(toolName) and not character:FindFirstChild(toolName) then
+            local tool = backpack:FindFirstChild(toolName)
+            if tool then
+                wait(0.1)  -- Wait a moment before equipping
+                character.Humanoid:EquipTool(tool)
+            end
+        end
+    end
+
+    -- Connect the equipTool function to the Backpack's ChildAdded event
+    player.Backpack.ChildAdded:Connect(equipTool)
+
+    -- Initial check to equip the tool if it's already in the backpack
+    equipTool()
+
+    -- Keep checking periodically (every second)
+    while true do
+        task.wait(1)
+        equipTool()
+    end
+end
 
 AutoEquip()
